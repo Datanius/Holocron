8c4ec9f58f90439bd4103478545bdfd22ac3556d
AntonBe2000 <anton.b@live.de>
2021-02-06 11:28:04 +0100

    header.html und footer.html erstellt und von index.php ausgelagert

--- SurveyJS/public/index.php
+++ SurveyJS/public/index.php
@@ -1,19 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Holocron - Beratung zu Preismodellen</title>
-    <meta charset="UTF-8">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="stylesheets/survey.css" />
-    <script src="scripts/survey.js"></script>
-    <style>
-        .factor {
-            display: flex;
-        }
-    </style>
-</head>
-<body>
+<?php include ("header.html"); ?>
 
 <script>
     window.questionnaire = <?php echo file_get_contents(__DIR__ . "/questionnaire.json"); ?>
@@ -24,6 +9,4 @@
 <script src="scripts/index.js"></script>
 <div id="surveyResult"></div>
 
-
-</body>
-</html>
+<?php include ("footer.html"); ?>
