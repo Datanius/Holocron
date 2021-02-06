3d615fc5832965366202c4659d1402b874a51277
AntonBe2000 <anton.b@live.de>
2021-02-06 11:24:15 +0100

    Titel in index.php angepasst

--- SurveyJS/public/index.php
+++ SurveyJS/public/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Welcome to JQuery</title>
+    <title>Holocron - Beratung zu Preismodellen</title>
     <meta charset="UTF-8">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
