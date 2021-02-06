78088c77645ed568f0048f34b0d553a42210db7d
AntonBe2000 <anton.b@live.de>
2021-02-06 13:13:12 +0100

    saveResults.php Alle Klassen mit require_once hinzugefügt

--- SurveyJS/public/actions/saveResults.php
+++ SurveyJS/public/actions/saveResults.php
@@ -1,11 +1,40 @@
 <?php
 
 require_once __DIR__ . "/../../src/Questions/Question.php";
-require_once __DIR__ . "/../../src/Questions/template_question_risk.php";
-require_once __DIR__ . "/../../src/Questions/RiskSharing.php";
 require_once __DIR__ . "/../../src/FactorObserver.php";
 require_once __DIR__ . "/../../src/Preismodell.php";
 require_once __DIR__ . "/../../src/Answer.php";
+
+//Questions reinladen
+//Kunden
+require_once __DIR__ . "/../../src/Questions/Kunden/Kunden_welche_art.php";
+//Kundennutzen
+require_once __DIR__ . "/../../src/Questions/Kundennutzen/Nutzen_wie_erfolgt.php";
+require_once __DIR__ . "/../../src/Questions/Kundennutzen/Nutzen_wie_haeufig.php";
+//Kundensituation
+require_once __DIR__ . "/../../src/Questions/Kundensituation/Kundensit_bisherig_geeignet.php";
+require_once __DIR__ . "/../../src/Questions/Kundensituation/Kundensit_budgetsituation.php";
+require_once __DIR__ . "/../../src/Questions/Kundensituation/Kundensit_kerngeschaeft_korreliert.php";
+require_once __DIR__ . "/../../src/Questions/Kundensituation/Kundensit_loesung_aufwaendig.php";
+require_once __DIR__ . "/../../src/Questions/Kundensituation/Kundensit_nutzergruppen.php";
+require_once __DIR__ . "/../../src/Questions/Kundensituation/Kundensit_planungssicherheit.php";
+require_once __DIR__ . "/../../src/Questions/Kundensituation/Kundensit_traegt_er_risiko.php";
+require_once __DIR__ . "/../../src/Questions/Kundensituation/Kundensit_tragen_sie_risiko.php";
+require_once __DIR__ . "/../../src/Questions/Kundensituation/Kundensit_zahlungsbereitschaft.php";
+require_once __DIR__ . "/../../src/Questions/Kundensituation/Kundensit_zusammenbrechen_service.php";
+//Markt
+require_once __DIR__ . "/../../src/Questions/Markt/Markt_eigenrealisierung.php";
+require_once __DIR__ . "/../../src/Questions/Markt/Markt_entwicklung.php";
+require_once __DIR__ . "/../../src/Questions/Markt/Markt_preismodell_alt.php";
+require_once __DIR__ . "/../../src/Questions/Markt/Markt_vergleichbare_produkte.php";
+//netzwerk
+require_once __DIR__ . "/../../src/Questions/Netzwerk/Netzwerk_gewinnbeteiligung.php";
+require_once __DIR__ . "/../../src/Questions/Netzwerk/Netzwerk_kunden_kooperation.php";
+//Technik
+require_once __DIR__ . "/../../src/Questions/Technik/Technik_erfassung_moeglich.php";
+require_once __DIR__ . "/../../src/Questions/Technik/Technik_hohe_fixkosten.php";
+require_once __DIR__ . "/../../src/Questions/Technik/Technik_welche_erfassung.php";
+
 
 $json = json_decode(trim(file_get_contents("php://input")), true);
 
