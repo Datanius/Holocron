7fc527855512ff93bec52b23f086c0311127aea7
AntonBe2000 <anton.b@live.de>
2021-02-13 14:26:15 +0100

    index.php Text hinzugefügt

--- SurveyJS/public/index.php
+++ SurveyJS/public/index.php
@@ -71,12 +71,18 @@
                 andere divs wenn nicht model in excluded z.B. in zeile 74
                 -->
 
+                <div class="flexbox">
+                    Durch Fragen ermittelter Faktor | Preismodelleignung nach Faktor
+                </div>
+                
                 <span x-on:click="tooltips[modell] = true" style="position:relative; cursor:pointer;">
                     <svg width="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                     <div x-show="tooltips[modell]" x-on:click.away="tooltips[modell] = false" class="tooltip">
                         <template x-for="[factor, factorValue] in Object.entries(factors)">
+
+
                             <div class="flexbox">
                                 <div style="width: 300px" x-text="factor"></div>
                                 <div class="progress-outer" style="flex: 1;">
