b5d548967fd7ef1455eb16efc16b379c2cd23c3c
AntonBe2000 <anton.b@live.de>
2021-02-17 10:16:03 +0100

    - calculate Funktion in Kundensit_risiko_enable.php hinzugefügt

--- SurveyJS/src/Questions/Kundensituation/Kundensit_risiko_enable.php
+++ SurveyJS/src/Questions/Kundensituation/Kundensit_risiko_enable.php
@@ -1,7 +1,14 @@
 <?php
 
 
-class Kundensit_risiko_enable
+class Kundensit_risiko_enable extends Question
 {
 
+
+    public function calculate($value, &$factors, $excluded)
+    {
+        // TODO: Implement calculate() method.
+
+        //Wie machen, dass die Frage nix macht?
+    }
 }
